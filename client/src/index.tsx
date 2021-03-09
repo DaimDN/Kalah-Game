3224e215a99a16253ab88cfc694eb87cfb5e6e3c
daimdn <abdul.daim26@gmail.com>
2021-03-09 16:27:22 +0000

    Added typescript to react client and installed react-router-dom and axios

---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Index() {
+    return (
+        <div>
+            <h1> welcome</h1>
+            
+        </div>
+    )
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
+
+
