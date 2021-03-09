afcd040613bcf7c8dd465b61e4bb8ca7440d3e3c
daimdn <abdul.daim26@gmail.com>
2021-03-09 16:23:17 +0000

    Added a client app and install Typescript and other libraries

--- server/src/main/java/com/example/kalah/Controller/Controller.java
+++ server/src/main/java/com/example/kalah/Controller/Controller.java
@@ -2,16 +2,22 @@ package com.example.kalah.Controller;
 
 
 import com.example.kalah.model.LandingPage;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-
+    @CrossOrigin(origins = "http://localhost:3000")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/", produces = "application/json")
     public static LandingPage LandingPage(){
-        LandingPage landingPage = new LandingPage("This is the backed");
+        LandingPage landingPage = new LandingPage("Welcome to Kalah Game");
         return landingPage;
     }
+
+
 }
