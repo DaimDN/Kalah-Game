f9a0e630996d8400d16764aa2466a66d52eaa5d0
daimdn <abdul.daim26@gmail.com>
2021-03-13 22:57:54 +0000

    Implemented JWT, BCrypt for creating Authentication and password protected routes

--- server/src/main/java/com/example/kalah/util/JsonWebToken.java
+++ server/src/main/java/com/example/kalah/util/JsonWebToken.java
@@ -20,7 +20,7 @@ public class JsonWebToken implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-    private String secret = "HRlELXqpSB";
+    private String secret = "daimadeveloper";
 
     public String generateToken(user userDetails) {
         Map<String, Object> claims = new HashMap<>();
